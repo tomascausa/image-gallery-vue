8d1fed5505ee2c9d508d406236c4e1cc646f9080
Tomas Causa <tomas@wcanvas.com>
2020-06-05 11:26:35 -0300

    Creating app state

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        token: null,
+        images: [],
+        selectedImage: null,
+        isLoading: false
+    }
+})
