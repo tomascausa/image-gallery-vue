a56f30e551dbe4141e552a8f02b46ee92788a7a1
Tomas Causa <tomas@wcanvas.com>
2020-06-05 11:38:28 -0300

    Creating Gallery skeleton

---
+++ src/components/Gallery.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="isLoading">
+        <div class="flex flex-wrap image-list mt-10 -p-3">
+            <div v-for="image in 10" :key="image.id" class="w-1/2 lg:w-1/5">
+                <v-sheet
+                    :color="`grey darken-2`"
+                    class="p-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    max-width="300"
+                    type="image"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+        </div>
+    </div>
+    <div v-else>
+        <div class="flex flex-wrap image-list mt-10 -p-3">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import axios from 'axios';
+import * as types from './../store/types';
+
+export default {
+    data () {
+      return {
+        page: this.$route.query.page ? +this.$route.query.page : 1,
+        pageCount: 1,
+        dialog: false,
+      }
+    },
+    computed: {
+        ...mapGetters({
+            images: types.IMAGES
+        }),
+        isLoading: {
+            get() {
+                return this.$store.getters[types.IS_LOADING];
+            },
+            set(value) {
+                this.$store.dispatch(types.UPDATE_IS_LOADING, value);
+            }
+        }
+    }
+}
+</script>
